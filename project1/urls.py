ecfe7302e66c10b654a2f5784eb0178d2db13b31
Daniel Sales <daniel.sales@petco.com>
2021-07-21 17:32:19 -0300

    first commit

---
+++ project1/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.db import router
+from django.urls import path, include
+from rest_framework import routers
+from escola.views import AlunosViewSet, CursosViewSet
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls))
+]
