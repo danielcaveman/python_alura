d006f414ad9f81fce116bcdf03f7b205b86c1f5e
Daniel Sales <daniel.sales@petco.com>
2021-08-04 14:45:27 -0300

    matricula route added

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,7 @@
+from django.db.models import query
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -11,3 +12,8 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer    
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
